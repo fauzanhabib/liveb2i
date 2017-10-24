761de5431ca3168d75608ee7bdbc88c8c6d25c16
Devan <devananda@dyned.com>
2017-10-24 11:01:39 +0700

    match partner with new student partner role

--- live/application/models/partner_model.php
+++ live/application/models/partner_model.php
@@ -16,12 +16,13 @@ class partner_model extends MY_Model {
           
 
         public function get_student_supplier($id = ''){
+            $role = array(5,8);
             $this->db->distinct();
             $this->db->select("p.id, p.profile_picture, p.name, p.address, p.city, p.state, p.zip, p.country");
             $this->db->from('partners p');
             $this->db->join('user_profiles up', 'p.id = up.partner_id');
             $this->db->join('users u', 'u.id = up.user_id');
-            $this->db->where('u.role_id', 5);
+            $this->db->where_in('u.role_id', $role);
             if($id){
                 $this->db->where('p.admin_regional_id', $id);
             }
