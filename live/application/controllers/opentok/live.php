b41634b8584151211afe37d9a6f747bbbaad656d
Irawan <idwihastomo@dyned.com>
2018-03-12 14:39:59 +0700

    Fix mastery test color

--- live/application/controllers/opentok/live.php
+++ live/application/controllers/opentok/live.php
@@ -428,7 +428,7 @@ class Live extends MY_Site_Controller {
             $max_buletan_student = sizeof(@$gsp->data->study->mastery_tests);
 
             for($l=0;$l<$max_buletan_student;$l++){
-              $mt_color['mt'.$k] = @$mt_status_to_colour[@$gsp->data->coach->sessions[$l]->status];
+              $mt_color['mt'.$k] = @$mt_status_to_colour[@$gsp->data->study->mastery_tests[$l]->status];
               $k++;
             }
 
