861f97c4803de01693bab09bfcaa960863ac58e7
irawanwimo <irawandwihastomo@ymail.com>
2018-05-21 16:37:23 +0700

    Change jquery sources

--- live/application/views/contents/opentok/coach/session.php
+++ live/application/views/contents/opentok/coach/session.php
@@ -34,7 +34,8 @@
     return "If you close this page, you will not get a token from this session.";
   };
 </script>
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.2.3.min.js"></script>
+<!-- <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.2.3.min.js"></script> -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 <script src="<?php echo base_url();?>assets/js/script.js"></script>
 
 <script>
--- live/application/views/contents/opentok/student/session_agora.php
+++ live/application/views/contents/opentok/student/session_agora.php
@@ -13,7 +13,8 @@ if(@$user_extract2){
 
 ?>
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/main.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.2.3.min.js"></script>
+<!-- <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.2.3.min.js"></script> -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 <script src="<?php echo base_url();?>assets/js/script.js"></script>
 <!-- <script src="<?php echo base_url();?>assets/js/AgoraRTCSDK-2.1.1.js"></script> -->
 <script src="<?php echo base_url();?>assets/js/AgoraRTCSDK-2.2.0.js"></script>
