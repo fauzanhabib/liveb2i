ef91496fb12dad858f66f16f6f36db8a2de311ed
irawanwimo <irawandwihastomo@ymail.com>
2018-06-12 13:54:11 +0700

    Make rating be 2 decimal point on reporting

--- live/application/views/default/contents/admin/reporting/coach/coachsummary.php
+++ live/application/views/default/contents/admin/reporting/coach/coachsummary.php
@@ -513,7 +513,7 @@
                             if(@$rateaverage == 0){
                                 echo '';
                             }else{
-                                echo $rateaverage;
+                                echo round($rateaverage, 2);
                             }
                         ?></td>
                     </tr>
