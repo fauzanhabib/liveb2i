a6a88ccb3395d13b9909eb9494493c2bb856969e
Devan <devananda@dyned.com>
2018-08-23 15:13:59 +0700

    Live-821

--- live/application/views/default/contents/student_partner_neo/reporting/studentreport.php
+++ live/application/views/default/contents/student_partner_neo/reporting/studentreport.php
@@ -211,6 +211,7 @@
                 $a = 1;
                 $no = 1;
                 $nowdate  = date("Y-m-d");
+                $refustatus = array(13,21,23);
 
                 foreach ($stu_rpt as $d) {
 
@@ -220,6 +221,12 @@
                                         ->from('token_histories')
                                         ->where('user_id',$d->user_id)
                                         ->where('token_status_id',1)
+                                        ->get()->result();
+
+                        $token_not_usage = $this->db->select('token_amount')
+                                        ->from('token_histories')
+                                        ->where('user_id',$d->user_id)
+                                        ->where_in('token_status_id',$refustatus)
                                         ->get()->result();
 
                         $token_balance = $this->db->select('token_amount')
@@ -270,6 +277,14 @@
                                         ->where('token_status_id',1)
                                         ->order_by('transaction_date','DESC')
                                         ->get()->result();
+
+                        $token_not_usage = $this->db->select('token_amount')
+                                            ->from('token_histories')
+                                            ->where('transaction_date >=',$from)
+                                            ->where('user_id',$d->user_id)
+                                            ->where_in('token_status_id',$refustatus)
+                                            ->order_by('transaction_date','DESC')
+                                            ->get()->result();
 
                         $token_balance = $this->db->select('*')
                                         ->from('token_histories')
@@ -324,6 +339,15 @@
                                         ->where('token_status_id',1)
                                         ->order_by('transaction_date','DESC')
                                         ->get()->result();
+
+                        $token_not_usage = $this->db->select('token_amount')
+                                            ->from('token_histories')
+                                            ->where('transaction_date >=',$from)
+                                            ->where('transaction_date <=',$to)
+                                            ->where('user_id',$d->user_id)
+                                            ->where_in('token_status_id',$refustatus)
+                                            ->order_by('transaction_date','DESC')
+                                            ->get()->result();
 
                         $token_balance = $this->db->select('*')
                                         ->from('token_histories')
@@ -387,6 +411,12 @@
                          $sum += $value->token_amount;
                     }
 
+                    $sub = 0;
+                    foreach($token_not_usage as $key=>$value){
+                      if(isset($value->token_amount))
+                         $sub += $value->token_amount;
+                    }
+
 
                     $app_id="";
                     foreach($appid as $ap){
@@ -434,8 +464,8 @@
                         <td><?php if(@$addeds){echo @$addeds;}else{echo '0';} ?></td>
                         <td>
                             <?php if($sum != 0){ ?>
-                            <?php echo $sum; ?>
-                            <?php }else{ echo $sum; }?>
+                            <?php echo $sum - $sub; ?>
+                            <?php }else{ echo $sum - $sub; }?>
                         </td>
                         <td><?php echo @$tokenbal; ?></td>
                         <td>
